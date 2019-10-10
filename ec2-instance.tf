03557477738868022ab310dc87fe64732f12d819
Gennady Saburov <saburovga@users.noreply.github.com>
2019-10-10 11:21:52 +0300

    fixing syntax

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2-exercise1" {
     ami = "ami-011b3ccf1bd6db744"
     instance_type = "t2.micro"
-    tags {
+    tags = {
         Name="ec2-exercise1"
     }
     key_name="my_key"
