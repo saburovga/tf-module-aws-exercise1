c78a50305f115d3d7469b6025eec2110899a178b
Gennady Saburov <saburovga@gmail.com>
2019-10-23 12:27:00 +0300

    uploading public key #2

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ resource "null_resource" "awscli_install" {
   }
   provisioner "local-exec" {
     command = <<EOH
-ls -l ./keys
+ls -l ~
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -b ~/bin/aws
