23d89cde58a1a3dd45e6d636e3c67a82ec48c004
Gennady Saburov <saburovga@gmail.com>
2019-10-23 14:34:36 +0300

    creating windows instance #1

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,30 @@
 resource "aws_instance" "ec2-exercise1" {
-    ami = "ami-04b9e92b5572fa0d1"
-    instance_type = "t2.micro"
+    ami = "ami-0d4df21ffeb914d61"
+    instance_type = "t2.medium"
     tags = {
         Name="ec2-exercise1"
     }
     key_name="test"
+    get_password_data=true
 	
     vpc_security_group_ids = ["sg-a736e4e5"]
 	subnet_id = "subnet-234fd544"
+
+    root_block_device {
+        delete_on_termination=true
+        volume_type="gp2"
+        volume_size=40
+        iops=100
+    }
+
+    user_data=<<EOF
+<powershell>
+Write-Output "y\n"|winrm quickconfig
+Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+winrm set winrm/config/service/Auth '@{Basic="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="1024"}'
+</powershell>
+  EOF
 }
 
