4d46df6d6830108d95f31f3eb3b9fbc690759aaf
Gennady Saburov <saburovga@gmail.com>
2019-10-23 11:47:35 +0300

    installing pip #5

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 
 resource "null_resource" "awscli_install" {
   triggers = {
-      trigger = "4"
+      trigger = "5"
   }
   provisioner "local-exec" {
     command = <<EOH
@@ -14,6 +14,7 @@ curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -b ~/bin/aws
 export PATH=~/bin:$PATH
+aws --version
 EOH
   }
 }
